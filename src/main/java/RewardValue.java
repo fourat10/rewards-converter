7a067adf8238db55a6988857497daa4dae593f00
fourat <fouratmarouen0@gmail.com>
2024-08-17 16:39:01 +0100

    change miles to integer

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cash;
+    private int miles;
+    private static final float MILES_TO_CASH_RATE = 0.0035f;
+    public RewardValue(double cashvalue){
+        cash = cashvalue;
+        this.miles =(int) (this.cash / MILES_TO_CASH_RATE) ;
+    }
+    public RewardValue(int milesvalue){
+        miles = milesvalue;
+        this.cash =  (this.miles*MILES_TO_CASH_RATE);
+    }
+    float getMilesValue(){
+        return this.miles ;
+    }
+    double getCashValue(){
+        return this.cash ;
+    }
+
 }
